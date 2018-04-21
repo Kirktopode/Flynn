f966239d97845cdff7f3ab57ac1100285d06ffb4
Kirktopode <masksofpurple@gmail.com>
2018-04-20 18:42:51 -0600

    backup the stupid thing

--- logger.py
+++ logger.py
@@ -1,1 +1,30 @@
-import bme280.py
+from bme280 import readBME280All
+from mpu6050 import mpu6050
+
+import os
+import sys
+import csv
+import smbus
+import time
+
+mpu = mpu6050()
+bus = smbus.SMBus(1)
+nano_address = 0x04
+
+def write_nano(value):
+    bus.write_byte(address, value)
+    return -1
+
+def read_nano():
+    number = bus.read_byte(address)
+    return number
+
+while True:
+
+    writeNumber(var)
+    print ("RPI: Hi Arduino, I sent you", var)
+    # sleep one second
+    time.sleep(1)
+
+    number = readNumber()
+    print ()
